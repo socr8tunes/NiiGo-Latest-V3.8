b41150eebe1ac33dd5fb30c4e87e02b6c0019d5d
socr8tunes <38995355+socr8tunes@users.noreply.github.com>
2026-04-28 11:25:51 +0000

    Comment out unused import for taxi_order_shimmer_widget
    Comment out the import for taxi_order_shimmer_widget.dart.

--- lib/features/ride_share_module/ride_order/widgets/ride_order_view_widget.dart
+++ lib/features/ride_share_module/ride_order/widgets/ride_order_view_widget.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:sixam_mart/common/widgets/custom_ink_well.dart';
 import 'package:sixam_mart/common/widgets/no_data_screen.dart';
 import 'package:sixam_mart/common/widgets/paginated_list_view.dart';
-import 'package:sixam_mart/features/rental_module/rental_order/widgets/taxi_order_shimmer_widget.dart';
+//import 'package:sixam_mart/features/rental_module/rental_order/widgets/taxi_order_shimmer_widget.dart';
 import 'package:sixam_mart/features/ride_share_module/ride_order/controllers/ride_controller.dart';
 import 'package:sixam_mart/features/ride_share_module/ride_order/screens/ride_order_complete_screen.dart';
 import 'package:sixam_mart/features/ride_share_module/ride_payment/screens/ride_payment_screen.dart';
